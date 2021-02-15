c4e6c24a0fc4206692c00eb4fb0862d3ed78a823
Zack Werner <allthelight044@gmail.com>
2021-02-15 15:39:43 -0600

    my first react code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+// App is a Component
+// Components are functions that return JSX
+//
 function App() {
+  let name = 'Zack'; // we can use {} similar to ${} and fill inside
+
+  let myH1 = <h1> Go home, {name}, you're drunk!!! </h1>; // {name}
+
+  // "HTML" is actually "JSX"
+  // "JavaScript extended"
+  // as in App.jsx
+
+  // client side (react) we'll do:
+  import HelloCullen from './HelloCullen';
+
   return (
     <div className="App">
+      {myH1}
+
+      {/*Hello Cullen is a React component*/}
+      <HelloCullen />
+      <HelloCullen />
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/HelloCullen.jsx
@@ -0,0 +1,16 @@
+function HelloCullen() {
+  
+
+  // JSX fragment!
+  // an element with no name
+  return (
+    <>
+    <h1>Hello World </h1>
+    <h2> See ya later </h2>
+    </>
+
+  ); 
+
+}
+
+export default HelloCullen;
